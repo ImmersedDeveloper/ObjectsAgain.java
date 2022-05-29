c01ed28ad335a264d0362be7a6b5c93e8abd1aa1
김주형 <wnguddl96@knou.ac.kr>
2022-05-30 05:25:34 +0900

    Add files via upload

--- Objects/src/main/java/TicketSeller.java
+++ Objects/src/main/java/TicketSeller.java
@@ -1,2 +1,11 @@
 public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
 }
