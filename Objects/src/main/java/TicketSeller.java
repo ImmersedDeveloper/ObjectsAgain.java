2aa8f744427960107bd7b8feed05ae9ec51c2016
김주형 <wnguddl96@knou.ac.kr>
2022-06-01 05:30:01 +0900

    Add files via upload

--- Objects/src/main/java/TicketSeller.java
+++ Objects/src/main/java/TicketSeller.java
@@ -4,10 +4,15 @@ public class TicketSeller {
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
-
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
-    }
+/**
+ * 제거에 주목 !!
+    public TicketOffice getTicketOffice() { ticketOffice는 TicketSeller 안에만 존재,
+ TicketSeller는 ticketOffice와 연관된 역할을 스스로 수행할 수 밖에 없음.
+ 즉, Theater는 ticketOffice에 접근 불가, TicketSeller 내부에 존재한다는 사실 알지 못함.
+ 오직 sellTo 메시지만 이해, 응답!!
+       return ticketOffice;
+  }
+ */
 
     public void sellTo(Audience audience){
         if(audience.getBag().hasInvitation()){
